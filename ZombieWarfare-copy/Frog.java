341ab1a4662289b8a522bf954984f31a1f2fb70a
Poothapillai Abisan <abisan422@gmail.com>
2020-11-06 18:23:39 -0500

    First Change

---
+++ ZombieWarfare-copy/Frog.java
@@ -0,0 +1,19 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Frog extends Actor
+{
+
+    /**
+     * Act - do whatever the Frog wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    final public void act()
+    {
+        move(5);
+    }
+}
